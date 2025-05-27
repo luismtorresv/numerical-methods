054b9bc06c7db11f62aa13b59f3b0a395e9cf13b
Luis Torres <lmtorresv@eafit.edu.co>
2025-05-27 13:09:39 -0500

    Change bisection to use a `Result`

--- single_variable/bisection.py
+++ single_variable/bisection.py
@@ -5,152 +5,116 @@ import sympy as sp
 from utils.general import nm_lambdify
 from utils.interface_blocks import calculate_tolerance, graph, ui_input_function
 
+from .common import Result, Table, calculate_error, determine_error_type
 from .report import generate_report
 
 
-def bisection(a, b, niter, tol, tolerance_type, function):
+def bisection(a, b, niter, tol, tolerance_type, function) -> Result:
+    result = Result()
+    table = Table()
+    error_type = determine_error_type(tolerance_type)
+
     if b < a:
-        return {
-            "status": "error",
-            "message": "Invalid Arguments, b must be greater than a.",
-        }
+        result.error_message = "Invalid Arguments, b must be greater than a."
+        return result
 
-    table = []
-    row = {}
-    error_type = (
-        "Relative Error"
-        if tolerance_type == "Significant Figures"
-        else "Absolute Error"
-    )
-
-    # initial call
-    row["a"] = a
-    row["x_n"] = b
-    row["mid"] = (a + b) / 2
-    row["f(a)"] = function(a)
-    row["f(mid)"] = function((a + b) / 2)
-    row["f(b)"] = function(b)
-    row["Error"] = None
-    table.append(row)
-    err = 100
+    table.add_row(a, function(a), None)
+    error = 100  # Arbitrary initial error
 
     if function(a) * function(b) > 0:
-        return {
-            "status": "error",
-            "message": "Invalid Arguments, the function does not change sign in the interval.",
-        }
-    else:
+        result.error_message = (
+            "Invalid Arguments, the function does not change sign in the interval."
+        )
+        return result
+
+    mid = (a + b) / 2
+    i = 0
+    while i < niter and error > tol:
+        if function(a) * function(mid) <= 0:
+            b = mid
+        else:
+            a = mid
+
+        i += 1
+        prev_mid = mid
         mid = (a + b) / 2
-        iterations = 0
-        while iterations < niter and err > tol:
-            if function(a) * function(mid) <= 0:
-                b = mid
-            else:
-                a = mid
-            iterations += 1
-            prev_mid = mid
-            mid = (a + b) / 2
 
-            row = {}
-            row["a"] = a
-            row["x_n"] = b
-            row["mid"] = mid
-            row["f(a)"] = function(a)
-            row["f(mid)"] = function(mid)
-            row["f(b)"] = function(b)
-            if error_type == "Relative Error":
-                row["Error"] = abs((mid - prev_mid) / mid)
-            else:
-                row["Error"] = abs(mid - prev_mid)
-            err = row["Error"]
-            table.append(row)
+        error = calculate_error(mid, prev_mid, error_type)
+        table.add_row(mid, function(mid), error)
 
-        df = pd.DataFrame(table)
-        return {"status": "success", "table": df}
+    df = table.as_dataframe()
+    result.table = df
+    result.set_success_status()
+    return result
 
 
 def show_bisection():
-    try:
-        st.header("Bisection Method")
+    st.header("Bisection Method")
 
-        function_input = ui_input_function(placeholder_function="x**2 - 4")
+    function_input = ui_input_function(placeholder_function="x**2 - 4")
+
+    col1, col2 = st.columns(2)
+    with col1:
+        a = st.number_input(
+            "Initial point of search interval (a)",
+            format="%.4f",
+            value=0.1,
+            step=0.0001,
+        )
+    with col2:
+        b = st.number_input(
+            "End point of search interval (b)",
+            format="%.4f",
+            value=3.0,
+            step=0.0001,
+        )
+
+    tol, niter, tolerance_type = calculate_tolerance()
+    st.markdown(f"**Calculated Tolerance:** {tol:.10f}")
+
+    x = sp.symbols("x")
+    function_sp = sp.sympify(function_input)
+    first_derivative = sp.diff(function_sp, x)
+    second_derivative = sp.diff(first_derivative, x)
+
+    st.subheader("Function")
+    st.latex(f"f({x}) = {sp.latex(function_sp)}")
+
+    function = nm_lambdify(function_input, x)
+
+    result = bisection(a, b, niter, tol, tolerance_type, function)
+
+    st.subheader("Results")
+    if result.has_failed():
+        st.error(result.error_message)
+
+    result_display = result.table.style.format("{:.15e}")
+
+    st.divider()
+
+    st.header("Result")
+    if not result.has_failed():
+        last_x = result.table.iloc[-1]["x"]
+        st.success(":material/check: Root found.")
 
         col1, col2 = st.columns(2)
-        with col1:
-            a = st.number_input(
-                "Initial point of search interval (a)",
-                format="%.4f",
-                value=0.1,
-                step=0.0001,
-            )
-        with col2:
-            b = st.number_input(
-                "End point of search interval (b)",
-                format="%.4f",
-                value=3.0,
-                step=0.0001,
-            )
+        col1.metric("$x$", f"{last_x:.10e}")
+        col2.metric("$f(x)$", f"{function(last_x):.10e}")
 
-        tol, niter, tolerance_type = calculate_tolerance()
-        st.markdown(f"**Calculated Tolerance:** {tol:.10f}")
+    st.subheader("Table")
+    st.table(result_display)
 
-        x = sp.symbols("x")
-        function_sp = sp.sympify(function_input)
-        first_derivative = sp.diff(function_sp, x)
-        second_derivative = sp.diff(first_derivative, x)
+    st.divider()
 
-        st.subheader("Function")
-        st.latex(f"f({x}) = {sp.latex(function_sp)}")
+    graph(function_input)
 
-        function = nm_lambdify(function_input, x)
-
-        # DO CHECKS ON INPUT INTEGRITY
-        # check if derivative is continuous in general
-
-        result = bisection(a, b, niter, tol, tolerance_type, function)
-
-        if result["status"] == "error":
-            st.error(result["message"])
-            graph(function_input)
-            return
-        else:
-            result = result["table"]
-
-        # Add a slider to choose the number of decimals to display
-        decimals = st.slider(
-            "Select number of decimals to display on table",
-            min_value=1,
-            max_value=10,
-            value=4,
-            help="Adjust the number of decimal places for the result table.",
-        )
-        # Format the dataframe to display the selected number of decimals
-        result_display = result.style.format(
-            f"{{:.{decimals}f}}"
-        )  # Use f-string to format dynamically
-
-        st.subheader("Results")
-        st.dataframe(result_display, use_container_width=True)
-
-        mid = result.iloc[-1]["mid"]
-        if function(mid) < 0 + tol:
-            st.success(
-                f"Root found at x = {mid:.{decimals}f}: f({mid:.{decimals}f}) = {function(mid):.{decimals}f}"
-            )
-        else:
-            st.warning(
-                f"Method did not converge, potentially because of a discontinuity in the function."
-            )
-        graph(function_input)
-        generate_report(
-            niter,
-            function,
-            tol,
-            tolerance_type,
-            x,
-            first_derivative,
-            second_derivative,
-        )
-    except Exception as e:
-        st.error("Error: Check your inputs ")
-        print(e)
+    st.divider()
+    generate_report(
+        niter,
+        function,
+        tol,
+        tolerance_type,
+        x,
+        first_derivative,
+        second_derivative,
+    )
